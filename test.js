41ba2acd5456bcff1bb246828f9733588900869f
snooze26 <langdon.douglas26@gmail.com>
2025-04-10 20:20:02 -0500

    feat: added tests for calculator object

--- test.js
+++ test.js
@@ -8,3 +8,19 @@ test('Capatlizes the string' , () => {
 test('Reverses the string', () => {
     expect(reverseReverse('12345')).toBe('54321')
 })
+
+test('1 + 1 = 2' , () => { 
+    expect(sum(1+1)).toBe(2);
+})
+
+test('2 -1 = 1' , () => { 
+    expect(sub(2-1)).toBe(1);
+})
+
+test('2 * 5 = 10' , () => { 
+    expect(times(2*5)).toBe(10);
+})
+
+test('10 / 2 = 5' , () => { 
+    expect(times(10 / 2 )).toBe(5);
+})
