2d99d6ce8a6f95339bf4ec2cdae8ef31a8703cff
snooze26 <langdon.douglas26@gmail.com>
2025-04-10 20:11:11 -0500

    feat: added reverse string test

--- test.js
+++ test.js
@@ -4,3 +4,7 @@ import { upperCase } from './upperCase.js';
 test('Capatlizes the string' , () => {
     expect(upperCase('abcdefg')).toBe('ABCDEFG');
 })
+
+test('Reverses the string', () => {
+    expect(reverseReverse('12345')).toBe('54321')
+})
