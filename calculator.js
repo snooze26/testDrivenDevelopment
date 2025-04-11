4bcda0fc9e8a722715dc273ffb2bb1cee62e5d58
snooze26 <langdon.douglas26@gmail.com>
2025-04-10 20:35:11 -0500

    feat: finished adding the calc object and testing

--- calculator.js
+++ calculator.js
@@ -1,17 +1,18 @@
-export class Calculator { 
+export const Calculator = { 
     add(a , b) { 
         return a + b
-    }
+    },
 
     sub(a , b) { 
         return a - b 
-    }
+    },
 
     times(a , b) { 
         return a * b 
-    }
+    },
 
     divide(a , b) {
         return a / b 
     }
 }
+
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // retrieving the functions
 import { upperCase } from './upperCase.js';
 import { reverseReverse } from './reverseReverse.js';
-import { Calculator } from './calculator.js';
+import { Calculator  } from './calculator.js';
 
 // test('Capatlizes the string' , () => {
 //     expect(upperCase('abcdefg')).toBe('ABCDEFG');
@@ -12,17 +12,17 @@ import { Calculator } from './calculator.js';
 // })
 
 test('1 + 1 = 2' , () => { 
-    expect(sum(1+1)).toBe(2);
+    expect(Calculator.add(1 , 1)).toBe(2);
 })
 
 test('2 -1 = 1' , () => { 
-    expect(sub(2-1)).toBe(1);
+    expect(Calculator.sub(2 , 1)).toBe(1);
 })
 
 test('2 * 5 = 10' , () => { 
-    expect(times(2*5)).toBe(10);
+    expect(Calculator.times(2 , 5)).toBe(10);
 })
 
 test('10 / 2 = 5' , () => { 
-    expect(times(10 / 2 )).toBe(5);
+    expect(Calculator.divide(10 , 2 )).toBe(5);
 })
