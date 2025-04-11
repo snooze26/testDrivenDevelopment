0b8e04d1ba081b9d67413bc83d54c3d28c75d398
snooze26 <langdon.douglas26@gmail.com>
2025-04-10 20:23:18 -0500

    added calculator object

---
+++ calculator.js
@@ -0,0 +1,17 @@
+export class Calculator { 
+    add(a , b) { 
+        return a + b
+    }
+
+    sub(a , b) { 
+        return a - b 
+    }
+
+    times(a , b) { 
+        return a * b 
+    }
+
+    divide(a , b) {
+        return a / b 
+    }
+}
