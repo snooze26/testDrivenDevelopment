8c4daa11f5d77242e916dca55f189865efe2858b
snooze26 <langdon.douglas26@gmail.com>
2025-04-11 21:13:20 -0500

    feat: added checks for inputed values

--- ceasarCypher.js
+++ ceasarCypher.js
@@ -1,13 +1,21 @@
-export function ceaserCypher(string, shiftNum) { 
+import { error } from "node:console";
 
+export function ceaserCypher(string, shiftNum) { 
+    //check if string is string and shift number is a number 
+    if(typeof string !== "string") return console.error("Need to enter a valid string");
+    if(typeof shiftNum !== "number") return console.error("Need to enter a valid number for the shift");
+    
 // create empty result array
     let res = [] 
 // FOR each character in "string" get the ASCII code of that character 
     for(let i = 0; i < string.length; i+= 1){
         const asciiChar = string.charCodeAt(i);
-        console.log(asciiChar);
         // IF the character is NOT a letter (not in a-z or A-Z)
-        
+        if(
+            !(asciiChar >= 97 && asciiChar <= 122) || (asciiChar >= 65 && asciiChar <= 90)) {
+                console.log("if check is working");
+                console.log(asciiChar);
+            }
         // add the character as-is to result - WHY?  
         // Contintue to the next character 
 
@@ -29,7 +37,7 @@ export function ceaserCypher(string, shiftNum) {
     //RETURN all characters in result merged as a string 
 }
 
-const testCode = ceaserCypher("A", 1);
+const testCode = ceaserCypher("a", 5);
 console.log(testCode);
 
 
