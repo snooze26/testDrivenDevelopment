30f874f2e9f040f02cba0ff7da8a16dc6346c711
snooze26 <langdon.douglas26@gmail.com>
2025-04-11 21:01:30 -0500

    feat: wrote test for ceaserCyhper function

--- test.js
+++ test.js
@@ -11,18 +11,23 @@ import { Calculator  } from './calculator.js';
 //     expect(reverseReverse('12345')).toBe('54321')
 // })
 
-test('1 + 1 = 2' , () => { 
-    expect(Calculator.add(1 , 1)).toBe(2);
+// test('1 + 1 = 2' , () => { 
+//     expect(Calculator.add(1 , 1)).toBe(2);
+// })
+
+// test('2 -1 = 1' , () => { 
+//     expect(Calculator.sub(2 , 1)).toBe(1);
+// })
+
+// test('2 * 5 = 10' , () => { 
+//     expect(Calculator.times(2 , 5)).toBe(10);
+// })
+
+// test('10 / 2 = 5' , () => { 
+//     expect(Calculator.divide(10 , 2 )).toBe(5);
+// })
+
+test('The expected answer is abc' , () => { 
+    expect(Calculator.ceasarCypher("xyz", 3)).toBe("abc");
 })
 
-test('2 -1 = 1' , () => { 
-    expect(Calculator.sub(2 , 1)).toBe(1);
-})
-
-test('2 * 5 = 10' , () => { 
-    expect(Calculator.times(2 , 5)).toBe(10);
-})
-
-test('10 / 2 = 5' , () => { 
-    expect(Calculator.divide(10 , 2 )).toBe(5);
-})
