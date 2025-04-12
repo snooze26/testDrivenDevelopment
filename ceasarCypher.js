5d99894ce34a55a6014b9aa5e3c79832ac5dfd07
snooze26 <langdon.douglas26@gmail.com>
2025-04-11 21:48:58 -0500

    feat: created the function to take the characters and then shif them. (can only doing lowercase at the moment).

--- ceasarCypher.js
+++ ceasarCypher.js
@@ -10,37 +10,40 @@ export function ceaserCypher(string, shiftNum) {
 // FOR each character in "string" get the ASCAII code of that character 
     for(let i = 0; i < string.length; i+= 1) {
         const asciiChar = string.charCodeAt(i);
-        console.log("ASCII CHAR BEFORE IF STATEMENT0" , asciiChar)
         // IF the character is NOT a letter (not in a-z or A-Z)
         if(
             !(asciiChar >= 97 && asciiChar <= 122) || (asciiChar >= 65 && asciiChar <= 90)) {
                 // add the character as-is to result this is so we don't manipulate values such as spaces, "!", "-", etc.
                 res.push(asciiChar);
                 // Contintue to the next character 
-                console.log(res);
                 continue; 
             }
-        
 
-    // add shiftNum to the ASCII code and store as the cryptedChar WHY?
-
+        // add shiftNum to the ASCII code and store as the cryptedChar . this is my shift and create the new character 
+        let cryptedChar = shiftNum + asciiChar; 
+        console.log(cryptedChar);
     // IF the character is lowercase AND cryptedChar is beyond 'z' 
-        //wrap it back by subtracting 26 from the cryptedChar
-
+        if((cryptedChar > 122 && asciiChar >= 97 && asciiChar <= 122 ) ||
+           (cryptedChar > 90 && asciiChar >= 65 && asciiChar >= 65 && asciiChar <= 90)) {
+                //wrap it back by subtracting 26 from the cryptedChar
+                cryptedChar -= 26; 
+                console.log(cryptedChar); 
+            }
+            res.push(String.fromCharCode(cryptedChar));
+        
     // ELSE IF THE character is uppercase AND cryptedChar is beyond "Z"
         //wrap it back by subtracting 26 from cryptedChar
 
     // CONVERT the cryptedChar back to a character 
-    // Add it to the result array 
+   // Add it to the result array 
     }
-
+    //RETURN all characters in result merged as a string 
+    return res.join("");
 
     // end for loop
-
-    //RETURN all characters in result merged as a string 
 }
 
-const testCode = ceaserCypher("b", 5);
+const testCode = ceaserCypher("XYZ", );
 console.log(testCode);
 
 
